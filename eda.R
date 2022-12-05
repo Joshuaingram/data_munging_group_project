e66ef4c8485a56c7cb157add4e6ba68533ec21ea
dnezelek <111800160+dnezelek@users.noreply.github.com>
2022-12-05 16:27:53 -0500

    updated code for variables and plots

--- eda.R
+++ eda.R
@@ -7,13 +7,12 @@ setwd("C:/Users/nezel/Datamunging/group_project")
 data <- fread("flights_miami_large.csv")
 attach(data)
 head(data)
-daysInAdvance <- flightDate - searchDate
-departureHour <- as.integer(substr(anytime(as.numeric(substr(segmentsDepartureTimeEpochSeconds,1,10))),12,13))
-arrivalHour <- as.integer(substr(anytime(as.numeric(stri_extract(segmentsArrivalTimeEpochSeconds,regex="[0-9]{10}(?!\\|\\|)"))),12,13))
-layoverDurationInMinutes <- (1/60)*(as.integer(3600*as.numeric(stri_extract(travelDuration,regex="(?<=PT)[0-9]{1,2}(?=H)")) + 60*as.numeric(stri_extract(travelDuration,regex="(?<=H)[0-9]{1,2}(?=M)")) - sapply(lapply(str_split(segmentsDurationInSeconds,"\\|\\|"),as.numeric),sum)))
+data$daysInAdvance <- flightDate - searchDate
+data$departureHour <- as.integer(substr(anytime(as.numeric(substr(segmentsDepartureTimeEpochSeconds,1,10))),12,13))
+data$arrivalHour <- as.integer(substr(anytime(as.numeric(stri_extract(segmentsArrivalTimeEpochSeconds,regex="[0-9]{10}(?!\\|\\|)"))),12,13))
+data$layoverDurationInMinutes <- (1/60)*(as.integer(3600*as.numeric(stri_extract(travelDuration,regex="(?<=PT)[0-9]{1,2}(?=H)")) + 60*as.numeric(stri_extract(travelDuration,regex="(?<=H)[0-9]{1,2}(?=M)")) - sapply(lapply(str_split(segmentsDurationInSeconds,"\\|\\|"),as.numeric),sum)))
+data$firstAirline <- stri_extract(segmentsAirlineName, regex="[:alpha:]+(?!\\|\\|)")
 
-data$daysInAdvance <- daysInAdvance
-  
 latestFlights <- data %>%
   group_by(legId) %>%
   filter(daysInAdvance == 1)
@@ -27,8 +26,7 @@ ggplot(data = latestFlights, aes(x = totalFare)) +
 ggplot(data = latestFlights, aes(x = seatsRemaining)) +
   geom_histogram()
 
-#ggplot(data = latestFlights, aes(x = seatsRemaining, fill = stri_extract(segmentsAirlineName,regex="(?<=)[A-Z]+(?=\\|)"))) +
-#  geom_histogram()
+ggplot(data = latestFlights, aes(x = seatsRemaining, fill = firstAirline)) + geom_histogram()
 
 # Scatter plot
 # plot(daysInAdvance, totalFare, pch = 19, col = factor(seatsRemaining))
